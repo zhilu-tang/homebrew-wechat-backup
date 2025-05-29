8a55efb26b9a8122c305b0f427172244fd46b132
zhilu-tang <zhilu.tang@gmail.com>
2025-05-30 00:31:59 +0800

    v1.0.2 release

--- Formula/wechat-backup.rb
+++ Formula/wechat-backup.rb
@@ -51,6 +51,30 @@ class WechatBackup < Formula
     EOS
   end
 
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/wechat-backup</string>
+          <string>--service</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>StandardOutPath</key>
+        <string>/tmp/wechat-backup.log</string>
+        <key>StandardErrorPath</key>
+        <string>/tmp/wechat-backup.log</string>
+      </dict>
+      </plist>
+    EOS
+  end
+
   test do
     system "#{bin}/wechat-backup", "--version"
   end
